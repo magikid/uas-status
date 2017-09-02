fdc4b62c12e4c24e933cbd2a1e08af49bd02cc6f
Chris W Jones <chris@christopherjones.us>
2017-09-02 13:22:27 -0400

    Adds structs for various options in the Slack API

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 extern crate hyper;
+extern crate time;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
 
 use hyper::{Client};
+use std::io::Read;
+use time::Timespec;
+use serde_json::*;
+
+#[derive(Serialize,Deserialize,Debug)]
+enum Color{
+    Open,
+    Closed
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Attachment{
+    fallback: String,
+    ts: i32,
+    text: String,
+    color: Color
+}
+
+#[derive(Serialize,Deserialize, Debug)]
+struct Message{
+    username: String,
+    channel: String,
+    icon_url: String,
+    attachments: Vec<Attachment>
+}
 
 fn main() {
     let client = Client::new();
-    let url = "http://httpbin.org/status/201";
-    let mut response = match client.get(url).send() {
-        Ok(response) => response,
-        Err(_) => panic!("Problem requesting"),
-    };
+    let uri = "https://hooks.slack.com/services/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";
+    let mut response = client.post(uri)
+        .body("param=hi")
+        .send()
+        .unwrap();
     let mut buf = String::new();
-    match response.read_to_string(&mut buf){
-        Ok(_) => (),
-        Err(_) => panic!("I give up"),
-    };
+    response.read_to_string(&mut buf).unwrap();
+    println!("status: {:?}", response.status_raw());
     println!("buf: {}", buf);
 }
